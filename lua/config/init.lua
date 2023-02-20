07445331b752aeacfa8df776938ee544236cfc95
YCSHome <3113929638@qq.com>
2023-02-20 21:00:21 +0800

    我也不记得更新了啥

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -72,6 +72,7 @@ hook(normal_mode, bind(utils.load_config, {
     backup            = true,
     updatetime        = 1000,
     signcolumn        = "yes",
+    cinoptions        = "g0l1Ls(0",
   }
 }))
 
@@ -97,6 +98,8 @@ hook(help_mode, bind(utils.load_config, {
   }
 }))
 
+local test = nil
+
 hook(cpp_buf_mode, function()
   vim.api.nvim_buf_set_keymap(0, "n", "<leader>b", ":w <CR> :cd %:h <CR> :bel 10sp term://" ..
   "g++ % -fsanitize=address -o %< && " ..
@@ -104,4 +107,56 @@ hook(cpp_buf_mode, function()
   "./%<" ..
   "<CR>"
   , {noremap = true, silent = true})
+end)
+
+hook(cpp_buf_mode, function()
+  vim.api.nvim_buf_set_keymap(0, "n", "<leader>n", "", {noremap = true, silent = true, callback = function()
+    if (test == nil) then
+      test = require("toggleterm.terminal").Terminal:new({
+        cmd = "runner " .. vim.fn.expand("%") .. " -fsanitize=address",
+        direction = "float",
+        start_in_insert = false,
+        dir = vim.fn.expand("%:h"),
+        float_opts = {
+          relative = "editor",
+          border = "rounded",
+          title = "hello world",
+          title_pos = "center",
+          row = 400,
+          col = 400,
+        },
+        close_on_exit = false,
+        on_exit = function(term, job, number, name)
+          local list = {
+            [0] = {"Accepted", "#00ff00"},
+            [1] = {"Compilation Error", "#ffff00"},
+            [2] = {"Wrong Answer", "#ff0000"},
+            [3] = {"Runtime Error", "#a020f0"},
+          }
+          if (list[number] ~= nil) then
+            vim.api.nvim_win_set_config(term.window, { title = list[number][1], title_pos = "center" })
+          else
+            vim.api.nvim_win_set_config(term.window, { title = "UKE", title_pos = "center" })
+          end
+          if (list[number] ~= nil) then
+            vim.api.nvim_set_hl(0, "FloatTitle", { fg=list[number][2] })
+          end
+        end,
+        on_open = function(term)
+          -- 获取全局UI属性
+          local ui = vim.api.nvim_list_uis()
+          local height = math.floor(ui[1].height * 0.4)
+          local width = math.floor(ui[1].width * 0.4)
+          vim.api.nvim_win_set_config(term.window, {
+            title = "Running...",
+            title_pos = "center",
+            height = height,
+            width = width,
+          })
+          -- 设置窗口的大小
+        end
+      });
+    end
+    test:toggle()
+  end})
 end)
---
+++ lua/pack/config/term.lua
@@ -0,0 +1,5 @@
+return {
+  lazy = true,
+  -- 当调用这个模块的时候加载这个插件
+  module = true,
+}
